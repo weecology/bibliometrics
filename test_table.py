0f2838548d6168869d8c7af02714ea01b7797691
Morgan Ernest <morgan@weecology.org>
2012-11-15 13:27:46 +0000

    moved citation processing code to function. Added code to calculate h_index, and total citations. Currently working on mean and median.

--- test_table.py
+++ test_table.py
@@ -19,7 +19,7 @@ def get_Scholarprofile(url):
 
 def extract_paperdata(profile_file):
     """parses out paper information from the profile html and outputs numpy array"""
-    bs_test=BeautifulSoup(test)
+    bs_test=BeautifulSoup(profile_file)
     table = bs_test.find("table", class_="cit-table")
     rows = table.findAll('tr')
     data = [[td.findChildren(text=True) for td in tr.findAll("td")] for tr in rows]
@@ -28,19 +28,39 @@ def extract_paperdata(profile_file):
     data_array = np.array(data)    
     return data_array
 
+def get_citations(paper_data):
+    """takes numpy array containing paper info, extracts citation #s and converts to int"""
+    citations = paper_data[:,2]
+    num_pubs = len(citations)
+    citations = [int(i[0]) for i in citations if i]
+    num_citedpubs = len(citations)
+    num_zeros = num_pubs-num_citedpubs
+    while num_zeros > 0:
+        citations.append(0)
+        num_zeros -= 1
+    return citations
+
+"""main code"""
 filename = "test_page.htm"
 profile = import_localfile(filename)
 publications = extract_paperdata(profile)
+citations = get_citations(publications)
 
 #Below this line is imperfect code
-citations = publications[:,2]
-num_pubs = len(citations)
-citations = [int(i[0]) for i in citations if i]
-num_citedpubs = len(citations)
-num_zeros = num_pubs-num_citedpubs
-while num_zeros > 0:
-    citations.append(0)
-    num_zeros -= 1
+citations.sort(reverse=True)
+h_index=0
+for item in citations:
+    if item > h_index:
+        h_index +=1
+total_citations=sum(citations)
+avg_cites=mean(citations)
+
+
+    
+
+
+    
+
     
 
 
