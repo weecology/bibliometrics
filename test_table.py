6027f99b22716b2c7143bb79876c2a125dcf4cf5
Morgan Ernest <morgan@weecology.org>
2012-11-16 22:52:20 +0000

    Fixed the >100 papers in a profile problem by rewriting the get_Scholarprofile function. Code now can import <700 papers, extract citation data, and combine with data from the Google_Scholars csv file.

--- test_table.py
+++ test_table.py
@@ -4,18 +4,34 @@ from bs4 import BeautifulSoup
 import numpy as np
 import re
 
-def import_localfile(filename):
+def import_ecologists(filename):
     """import local file containing a Google Scholar profile in html format"""
-    test_file=open(filename)
-    test=test_file.read()
-    test_file.close()
-    return test
+    reader = open(filename, 'r')
+    datareader = csv.reader(reader, delimiter=',')
+    data = []
+    for row in datareader:
+        data.append(row)
+    del data[0]
+    return data
 
 def get_Scholarprofile(url):
-    """Accessing googlescholarthrough url and modifying url to display more than default number of pubs"""
-    url_all=url+"&view_op=list_works&pagesize=100"
-    html = urlopen(url).read() 
-    return html
+    """Takes Scholar profile url and modifies display more than default number of pubs""" 
+    pattern=((".*user=(.*)&hl=en"))
+    user_id=re.search(pattern, url)
+    google_html="http://scholar.google.com/citations?hl=en&user=" + user_id.group(1) + "&view_op=list_works&pagesize=100"
+    html_file = urlopen(google_html).read()
+    pubs_from_html=extract_paperdata(html_file)
+    i=100
+    while i < 700:
+        str_i=str(i)
+        iterable_html="http://scholar.google.com/citations?hl=en&user=" + user_id.group(1) + "&pagesize=100&view_op=list_works&cstart=" + str_i
+        html_file=urlopen(iterable_html).read()
+        temp_pubs=extract_paperdata(html_file)
+        if len(temp_pubs) > 0:
+            pubs_from_html = np.vstack([pubs_from_html, temp_pubs])
+            i += 100
+        else:
+            return pubs_from_html
 
 def extract_paperdata(profile_file):
     """parses out paper information from the profile html and outputs numpy array"""
@@ -40,23 +56,36 @@ def get_citations(paper_data):
         num_zeros -= 1
     return citations
 
-"""main code"""
-filename = "test_page.htm"
-profile = import_localfile(filename)
-publications = extract_paperdata(profile)
-citations = get_citations(publications)
+def median(mylist):
+    sorts = sorted(mylist)
+    length = len(sorts)
+    if not length % 2:
+        return (sorts[length / 2] + sorts[length / 2 - 1]) / 2.0
+    return sorts[length / 2]
 
-#Below this line is imperfect code
-citations.sort(reverse=True)
-h_index=0
-for item in citations:
-    if item > h_index:
-        h_index +=1
-total_citations=sum(citations)
-avg_cites=mean(citations)
+def get_citation_metrics(citations):
+    citations.sort(reverse=True)
+    h_index = 0
+    for item in citations:
+        if item > h_index:
+            h_index += 1
+    total_citations = sum(citations)
+    avg_cites = sum(citations)/len(citations)
+    median_cites = median(citations) 
+    return h_index, total_citations, avg_cites, median_cites
+
+"""main code"""
+filename = "test_profiles.csv"
+ecologists = import_ecologists(filename)
+GS_profile = get_Scholarprofile(ecologists[0][1])
+citations = get_citations(GS_profile)
+h_index, total_citations, avg_cites, median_cites = get_citation_metrics(citations)
+
+scientist_data=[ecologists[0][0], ecologists[0][4], len(GS_profile), h_index, total_citations, avg_cites, median_cites]
 
 
     
+
 
 
     
