3637fccae4d76a264a9ab953e7f3498e6dc75c6c
Morgan Ernest <morgan@weecology.org>
2013-02-12 13:17:34 -0700

    refactored to make simpler keyword_link table
    Keyword/table refactored to create simple table linking keywords to
    subdiscipline, without userIDs

--- keyword_cleanup.py
+++ keyword_cleanup.py
@@ -7,35 +7,28 @@ import sqlite3 as dbapi
 import pandas as pd
 
 def get_keywords_fromdb():
-    """extracts names of institutions from institution table of citation_metrics 
-    database and returns values as a set"""
+    """extracts keywords from ecologist table of citation_metrics 
+    database and returns values"""
     con=dbapi.connect('citation_metric.sqlite')
     cur = con.cursor()
-    cur.execute("SELECT userID, keyword FROM ecologist_keywords")
+    cur.execute("SELECT keyword FROM ecologist_keywords")
     record = cur.fetchone()
     raw_keywords = []
     while record:
         if record:
             list_record = list(record)
-            raw_keywords.append(list_record)
+            raw_keywords.extend(list_record)
             record = cur.fetchone()   
     cur.close()
     con.close()
     return raw_keywords
 
-def notalready_in_database(record):
-    """Checks to see if institution has already been processed"""
+def notalready_in_database(keyword):
+    """Checks to see if user/keyword combo has already been processed"""
     processed_data = get_processeddata_fromdb()
     if processed_data:
-        userID = record[0]
-        keyword = record[1]
-        processed = pd.DataFrame(processed_data, columns=['UserID', 'ecologist_keyword'])
-        processed_users = set(processed['UserID'])
-        processed_keywords = set(processed['ecologist_keyword'])
-                             
-        if userID not in processed_users:
-            return True
-        elif keyword not in processed_keywords:
+        processed_keywords = set(processed_data)              
+        if keyword not in processed_keywords:
             return True
         else:
             return False
@@ -46,13 +39,13 @@ def get_processeddata_fromdb():
     """extracts userID and keyword from keyword_link table"""
     con=dbapi.connect('citation_metric.sqlite')
     cur = con.cursor()
-    cur.execute("SELECT userID, ecologist_keyword FROM keyword_link")
+    cur.execute("SELECT ecologist_keyword FROM keyword_link")
     record = cur.fetchone()
     processed_data = []
     while record:
         if record:
             list_record = list(record)
-            processed_data.append(list_record)
+            processed_data.extend(list_record)
             record = cur.fetchone()
     cur.close()
     con.close()
@@ -73,15 +66,17 @@ def quick_code_strip(record):
     return stringed
 
 def make_keyword_set(data):
-    dataframe = pd.DataFrame(data, columns=['UserID', 'keyword'])
-    unique_keywords = set(dataframe['keyword'])  
+    """returns a set of unique keywords that are in table ecologist_keywords"""
+    unique_keywords = set(data)  
     cleaned_keywords = []
     for item in unique_keywords:
         clean_keyword = quick_code_strip(item)
         cleaned_keywords.append(clean_keyword)
-    return cleaned_keywords
+    return set(cleaned_keywords)
 
 def create_keyword_dictionary(data):
+    """returns a dictionary linking the keyword from the ecologist_keywords table
+    with a standardized ecological subdiscipline"""
     keyword_dictionary = {}
     for item in keyword_set:
         ecology_patterns = (('.*[Cc]ommunity\s[Ee]cology.*'), 
@@ -113,38 +108,28 @@ def create_keyword_dictionary(data):
     return keyword_dictionary  
 
 def get_subdiscipline(item, dictionary):
-    try:
-        subdiscipline = dictionary[item]
-        return subdiscipline
-    except KeyError:
-        return False
-        
-    
+    subdiscipline = dictionary.get(item, False)
+    return subdiscipline      
+
+def make_keyword_subdiscipline_link(data_set, dictionary):
+    con=dbapi.connect('citation_metric.sqlite')
+    cur = con.cursor()
+    not_in_dictionary = []
+    for item in data_set:
+        not_already_processed = notalready_in_database(item)
+        if notalready_in_database:
+            subdiscipline = get_subdiscipline(item, dictionary)
+            if subdiscipline:
+                cur.execute ("INSERT INTO keyword_link VALUES(?,?)", (item, subdiscipline))
+    con.commit()
+    cur.close()
+    con.close()
+
 """main code"""
 keyword_data = get_keywords_fromdb()
 keyword_set = make_keyword_set(keyword_data)
 keyword_dictionary = create_keyword_dictionary(keyword_set)
+make_keyword_subdiscipline_link(keyword_set, keyword_dictionary) 
 
-con=dbapi.connect('citation_metric.sqlite')
-cur = con.cursor()
-for row in keyword_data:
-    not_already_processed = notalready_in_database(row)
-    if notalready_in_database:
-        subdiscipline = get_subdiscipline(row[1], keyword_dictionary)
-        if subdiscipline:
-            cur.execute ("INSERT INTO keyword_link VALUES(?,?,?)", (row[0], row[1], subdiscipline))
-con.commit()
-cur.close()
-con.close()
-
-    
-
-
-
-
-        
-        
-
-#make_csv("keyword.csv", cleaned_keywords)
 
 
